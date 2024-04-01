f25140b095797e3188f1b39eec2a80fb0480878b
Arnaud <arnaud.belliard@crea-inseec.com>
2024-04-02 01:48:11 +0200

    add input id non fonctionnel -in progress

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import Profile from "./components/Profile";
 import Navbar from "./components/Navbar";
 
 function App() {
-  const [character, setCharacter] = useState(null);
+  const [characterRefresh, setCharacterRefresh] = useState(null);
+  const [characterInput, setCharacterInput] = useState(null);
+  const [inputValue, setInputValue] = useState("");
 
   useEffect(() => {
     fetchRandomCharacter();
@@ -14,7 +16,7 @@ function App() {
     const randomCharacterId = Math.floor(Math.random() * 825) + 1; // Génère un ID de personnage aléatoire
     fetch(`https://rickandmortyapi.com/api/character/${randomCharacterId}`)
       .then((response) => response.json())
-      .then((data) => setCharacter(data))
+      .then((data) => setCharacterRefresh(data))
       .catch((error) => console.error("Error fetching data:", error));
   };
 
@@ -22,16 +24,54 @@ function App() {
     fetchRandomCharacter();
   };
 
+  //---------------------------Search----------------------------
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Dans handleSubmit" + inputValue);
+    fetch(`https://rickandmortyapi.com/api/character/?id=${inputValue}`)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.results.length > 0) {
+          setCharacterInput(data.results[0]);
+        } else {
+          alert("No character found");
+        }
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  };
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+    console.log("Dans handleChange" + inputValue);
+  };
+
   return (
-    <>
+    <div>
       <Navbar />
       <div className="profiles">
-        {character && <Profile {...character} />}
+        {characterRefresh && <Profile {...characterRefresh} />}
         <button className="btn-change" onClick={handleClick}>
           change Character
         </button>
       </div>
-    </>
+      {/* Rechcer grace a l'input */}
+      <div>
+        <h2>Search a character</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="number"
+            value={inputValue}
+            onChange={handleChange}
+            placeholder="Entrez quelque chose..."
+          />
+          <button type="submit">Send</button>
+        </form>
+        <div className="profiles">
+          {characterInput && <Profile {...characterInput} />}
+        </div>
+      </div>
+    </div>
   );
 }
 
